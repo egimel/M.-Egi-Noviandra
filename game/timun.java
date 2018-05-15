eb6b7a41486993501e1e5579f0267b5cee179545
adityaramadhan12 <adityasst71@gmail.com>
2018-05-15 09:54:54 +0800

    bebas

--- game/timun.java
+++ game/timun.java
@@ -12,8 +12,21 @@ public class timun extends Actor
      * Act - do whatever the timun wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int umur;
+    public timun(){
+        umur = 200; // 2 detik
+    }
+    public timun(int umur){
+        this.umur = umur; // 2 detik
+    }
     public void act() 
     {
         // Add your action code here.
-    }    
+        umur--;
+        if (isTouching(KANCIL.class))
+            getWorld().removeObject(this);
+        if (umur == 0)
+            getWorld().removeObject(this);
+        
+   }    
 }
